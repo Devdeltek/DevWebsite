cc76b15c54542552fbe28ae2b99a72c66db5baa0
Devdeltek <39629179+Devdeltek@users.noreply.github.com>
2022-09-29 22:03:54 -0700

    Setting up website

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express")
+//Ascii cats did not print correctly in the webpage, kept it in for the console
+const catMe = require('cat-me')
+const catFacts = require('get-cat-facts')
 
 const app = express()
 
@@ -7,6 +10,35 @@ app.set("view engine", "ejs")
 app.use(express.static("public"))
 
  
+
+//routes
+app.get("/", async (req, res) =>{
+
+console.log(catMe())
+let results = await catFacts.random([2])
+res.render("index",{"cat":results[0].text});
+
+});
+
+
+app.get("/java", async (req, res) =>{
+console.log(catMe())
+let results = await catFacts.random([2])
+res.render("java",{"cat":results[0].text});
+});
+
+app.get("/python", async (req, res) =>{
+console.log(catMe())
+let results = await catFacts.random([2])
+res.render("python",{"cat":results[0].text});
+});
+
+app.get("/swift", async (req, res) =>{
+console.log(catMe())
+let results = await catFacts.random([2])
+res.render("swift",{"cat":results[0].text});
+});
+
 
 
 app.listen(3000, () =>{
